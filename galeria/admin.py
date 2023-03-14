b9d805a0e2ad8827f1452edfc8fcda0de191083b
Luiz Gabriel Righetti de Campos <lgrdc.mt@gmail.com>
2023-03-13 23:07:01 -0300

    update - insert nem pages

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "categoria", "foto", "publicada")
+    list_display_links = ("id", "nome", "legenda")
+    search_fields = ("id", "nome")
+    list_filter = ("categoria", "publicada")
+    list_editable = ("categoria", "publicada",)
+    list_per_page = 10
+
+admin.site.register(Fotografia, ListandoFotografias)
